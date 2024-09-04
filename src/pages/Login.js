49b7ba9a22f3e74098e06a800409e98ecfa89784
Hieu Nguyen <hieu.nv2001hust@gmail.com>
2024-09-04 17:24:25 +0700

    Login page update

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
-import axios from "axios";
 import { useNavigate } from "react-router-dom";
-import bg from '../assets/bg.jpg';
+import bg from "../assets/bg.jpg";
 
 import {
   EuiForm,
@@ -10,6 +9,9 @@ import {
   EuiFieldPassword,
   EuiButton,
   EuiText,
+  EuiLink,
+  EuiSpacer,
+  EuiIcon,
 } from "@elastic/eui";
 import "@elastic/eui/dist/eui_theme_light.css";
 import { useLogin } from "../hooks/post";
@@ -19,7 +21,7 @@ const inputTextFieldStyle = {
 };
 
 const inputPasswordFieldStyle = {
-  width: "100%", // Sửa chiều rộng để đồng bộ với trường text
+  width: "100%",
 };
 
 const formRowStyle = {
@@ -37,7 +39,7 @@ const LoginForm = () => {
 
   const navigate = useNavigate();
 
-  const { mutate: login, isLoading, error } = useLogin();
+  const { mutate: login } = useLogin();
 
   const handleSubmit = async (event) => {
     event.preventDefault();
@@ -57,31 +59,73 @@ const LoginForm = () => {
   };
 
   return (
-    <div className="flex justify-center items-center" style={{height:"100vh",backgroundImage:`url(${bg})`,backgroundSize:'cover'}}>
-      <EuiForm component="form" onSubmit={handleSubmit} className="bg-white/50 px-40 py-20 rounded-2xl">
-        <EuiText style={{ color: "#83650a" }}>Đăng nhập</EuiText>
-        <EuiFormRow label="Email" style={formRowStyle}>
-          <EuiFieldText
-            name="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            placeholder="Enter your email"
-            style={inputTextFieldStyle}
-          />
-        </EuiFormRow>
-        <EuiFormRow label="Password" style={formRowStyle}>
-          <EuiFieldPassword
-            name="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            placeholder="Enter your password"
-            style={inputPasswordFieldStyle}
-          />
-        </EuiFormRow>
-        <EuiButton type="submit" fill style={buttonStyle}>
-          Log in
-        </EuiButton>
-      </EuiForm>
+    <div
+      className="flex flex-col justify-between items-center"
+      style={{
+        height: "100vh",
+        backgroundImage: `url(${bg})`,
+        backgroundSize: "cover",
+        position: "relative", // Thêm position relative để điều chỉnh vị trí của phần tử con
+      }}
+    >
+      {/* Đặt logo và tiêu đề vào góc trái trên cùng */}
+      <div
+        className="flex flex-row items-center space-x-2"
+        style={{
+          position: "absolute", // Sử dụng absolute để đưa phần tử này lên góc trái trên
+          top: "20px", // Điều chỉnh khoảng cách từ trên cùng
+          left: "20px", // Điều chỉnh khoảng cách từ trái
+        }}
+      >
+        <EuiIcon type="agentApp" size="xl" className="bg-white rounded-lg p-2" />
+        <p className="text-white text-2xl font-bold">SHLX</p>
+      </div>
+
+      {/* Phần tử này được đặt ở giữa */}
+      <div className="flex-grow flex items-center justify-center">
+        <EuiForm
+          component="form"
+          onSubmit={handleSubmit}
+          className="bg-white px-32 py-20 rounded-2xl flex flex-col items-center"
+        >
+          <EuiText>
+            <h2>ĐĂNG NHẬP</h2>
+          </EuiText>
+          <EuiFormRow label="EMAIL" style={formRowStyle} className="w-96">
+            <EuiFieldText
+              name="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              placeholder="Nhập email của bạn ..."
+              style={inputTextFieldStyle}
+            />
+          </EuiFormRow>
+          <EuiFormRow label="MẬT KHẨU" style={formRowStyle} className="w-96">
+            <EuiFieldPassword
+              value={password}
+              type="dual"
+              onChange={(e) => setPassword(e.target.value)}
+              aria-label="Use aria labels when no actual label is in use"
+              style={inputPasswordFieldStyle}
+              placeholder="Nhập mật khẩu của bạn ..."
+            />
+          </EuiFormRow>
+          <EuiButton type="submit" fill style={buttonStyle} className="w-52">
+            ĐĂNG NHẬP
+          </EuiButton>
+          <EuiSpacer />
+          <EuiLink>Quên tài khoản/ Mật khẩu ?</EuiLink>
+        </EuiForm>
+      </div>
+
+      {/* Phần tử này được đặt ở dưới cùng */}
+      <div className="flex justify-between items-center text-white w-full px-4 py-2">
+        <h3 className="text-xs">HỆ THỐNG QUẢN LÝ ĐÀO TẠO LÁI XE</h3>
+        <h3 className="text-xs">
+          Giải pháp của Toàn Phương SHLX. 0904.666.329 - 0982.911.000. Email:
+          shlx@toanphuong.com.vn
+        </h3>
+      </div>
     </div>
   );
 };
